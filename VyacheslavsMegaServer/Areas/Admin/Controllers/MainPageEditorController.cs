637193e6e7f8295973771f250a7534afe8c964af
Vya4slav0 <vya4slav4617@gmail.com>
2024-08-28 22:36:05 +0500

    Added transition to main page editor, added main page editor template

---
+++ VyacheslavsMegaServer/Areas/Admin/Controllers/MainPageEditorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VyacheslavsMegaServer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MainPageEditorController : Controller
+    {
+        public IActionResult EditMainPage()
+        {
+            return View();
+        }
+    }
+}
