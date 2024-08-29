88058cec8186d7de2a52c4f56b8f26e8be889fee
Vya4slav0 <vya4slav4617@gmail.com>
2024-08-29 21:43:44 +0500

    Done frontend to EditMainPage partition.

---
+++ VyacheslavsMegaServer/Models/Base/PageViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VyacheslavsMegaServer.Models.Base
+{
+    public abstract class PageViewModelBase
+    {
+        [Display(Name = "Заголовок страницы на вкладке браузера")]
+        public string PageTitle { get; set; }
+
+        [Display(Name = "Метатэг description")]
+        public string MetatagDescription { get; set; }
+
+        [Display(Name = "Метатэг keywords")]
+        public string MetatagKeywords { get; set; }
+    }
+}
--- VyacheslavsMegaServer/Models/MainPageViewModel.cs
+++ VyacheslavsMegaServer/Models/MainPageViewModel.cs
@@ -1,19 +1,26 @@
-﻿using VyacheslavsMegaServer.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using VyacheslavsMegaServer.Data.Entities;
 
 namespace VyacheslavsMegaServer.Models
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : Base.PageViewModelBase
     {
+        [Display(Name = "Заголовок")]
         public string Title { get; set; }
 
+        [Display(Name = "Текст ошибки")]
         public string ErrorMessage { get; set; }
 
+        [Display(Name = "Показать текст ошибки")]
         public bool ShowErrorMessage { get; set; }
 
+        [Display(Name = "Адрес сервера")]
         public string ServerAddress { get; set; }
-        
+
+        [Display(Name = "Жёлтая надпись")]
         public string YellowHint { get; set; }
-        
+
+        [Display(Name = "Описание проекта")]
         public string Description { get; set; }
         
         public Contact Creator { get; set; }
