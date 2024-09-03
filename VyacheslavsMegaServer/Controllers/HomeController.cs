869fc41e7601181b9bb964f90e6a933d68e73f72
Vya4slav0 <vya4slav4617@gmail.com>
2024-09-04 01:08:25 +0500

    Added page with status code, compressed images

--- VyacheslavsMegaServer/Controllers/HomeController.cs
+++ VyacheslavsMegaServer/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace VyacheslavsMegaServer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult StatusCodePage(int code)
+        {
+            return View("StatusCode", new StatusCodeViewModel(code));
+        }
     }
 }
