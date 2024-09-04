bd16a3bb4dcec3d656f524387dd8467d1bdd5ff8
Vya4slav0 <vya4slav4617@gmail.com>
2024-09-04 21:14:41 +0500

    Added service controller and qr image returning

---
+++ VyacheslavsMegaServer/Controllers/ServicesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VyacheslavsMegaServer.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult QR()
+        {
+            return File("~/img/siteQR.png", "image/png");
+        }
+    }
+}
