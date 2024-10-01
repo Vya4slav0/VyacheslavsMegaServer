b497ffac6ecbc76122161dfb8985fdd7b1b60ae9
Vya4slav0 <vya4slav4617@gmail.com>
2024-10-01 22:12:47 +0500

    Added frontend for donations

--- VyacheslavsMegaServer/Controllers/HomeController.cs
+++ VyacheslavsMegaServer/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace VyacheslavsMegaServer.Controllers
             return View(model);
         }
 
+        public IActionResult DonationPage() 
+        { 
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
